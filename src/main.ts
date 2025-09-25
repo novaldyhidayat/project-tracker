48a23733743042f5133ccfcd1f6f7bd9edd3dce6
Noval <novaldyhidayat@gmail.com>
2025-09-25 19:54:27 +0700

    fix: add cors validation

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   app.setGlobalPrefix('project-tracker');
 
   app.enableCors({
-    origin: true,
+    origin: ['https://novaldy-hidayat.my.id'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
